9e5a9b89de096b5758d93c23aabd7d9125fdc85d
Jacob Williams <jacobwilliams@users.noreply.github.com>
2021-05-01 18:41:48 -0500

    adding CI

---
+++ test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#
+# Just a simple test of the individual routines.
+#
+
+rm -rf bin
+rm -rf build
+mkdir bin
+mkdir build
+
+# ar cr libblas.a *.o
+
+# To solve an NLP using a sparse matrix data structure, the subroutines in the following files are required
+gfortran -O2 -std=legacy -c ./src/filterSD.f90 ./src/checkd.f90 ./src/glcpd.f90 ./src/l1sold.f90 ./src/shared.f90 ./src/schurQR.f90 ./src/sparseA.f90 ./src/util.f90
+ar cr ./build/nlp_sparse.a *.o
+rm *.o
+
+# To solve an NLP using a dense matrix data structure, the subroutines in the following files are required
+gfortran -O2 -std=legacy -c ./src/filterSD.f90 ./src/checkd.f90 ./src/glcpd.f90 ./src/l1sold.f90 ./src/shared.f90 ./src/denseL.f90 ./src/denseA.f90 ./src/util.f90      
+ar cr ./build/nlp_dense.a *.o
+rm *.o
+
+# To solve an LCP using a sparse matrix data structure, the subroutines in the following files are required
+gfortran -O2 -std=legacy -o ./src/glcpd.f90 ./src/shared.f90 ./src/util.f90 ./src/checkg.f90 ./src/schurQR.f90 ./src/sparseA.f90  
+ar cr ./build/lcp_sparse.a *.o
+rm *.o
+
+# To solve an LCP using a dense matrix data structure, the subroutines in the following files are required
+gfortran -O2 -std=legacy -o ./src/glcpd.f90 ./src/checkg.f90 ./src/shared.f90 ./src/denseL.f90 ./src/denseA.f90 ./src/util.f90
+ar cr ./build/lcp_dense.a *.o
+rm *.o
+
+# test programs:
+gfortran -O2 -std=legacy ./src/hs106.f90   ./build/nlp_sparse.a -o ./bin/hs106
+gfortran -O2 -std=legacy ./src/hs106d.f90  ./build/nlp_dense.a  -o ./bin/hs106d
+gfortran -O2 -std=legacy ./src/hs72.f90    ./build/nlp_sparse.a -o ./bin/hs72
+gfortran -O2 -std=legacy ./src/hs72d.f90   ./build/nlp_dense.a  -o ./bin/hs72d
