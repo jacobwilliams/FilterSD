71cc90f293d408c187747b5083d2e6078f1a4113
jacobwilliams <jacobwilliams@users.noreply.github.com>
2021-05-02 23:03:15 +0000

    Deploying to gh-pages from @ jacobwilliams/FilterSD@cc0078b2206cb5d186e390eda214a3da6e19f753 🚀

--- src/util.f90
+++ src/util.f90
@@ -1,22 +1,30 @@
-!Christen this file util.f
-
+!******************************************************************************************
+!>
+!  dense matrix utilities
+!
 !  Copyright (C) 1996 Roger Fletcher
-
+!
 !  Current version dated 26 May 2011
-
+!
 !  THE ACCOMPANYING PROGRAM IS PROVIDED UNDER THE TERMS OF THE ECLIPSE PUBLIC
 !  LICENSE ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION OF THE PROGRAM
 !  CONSTITUTES RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
 
-!*********************** dense matrix utilities ************************
+!    module utilities
+    
+!    contains
+!******************************************************************************************
 
-      subroutine rsol(n,nn,nmax,R,b)
-      implicit double precision (a-h,o-z)
-      dimension R(*),b(*)
+!******************************************************************************************
+!>
 !  solves Rx=b where R is nxn upper triangular. Solution overwrites b.
 !  R is a single suffix array: the first nmax elements contain the first row
 !  of R in positions 1:n, the next nmax-1 elements contain the second row of R,
 !  and so on. nn indexes the element R(n,n) (where nn=n*(3-n)/2+(n-1)*nmax)
+
+      subroutine rsol(n,nn,nmax,R,b)
+      implicit double precision (a-h,o-z)
+      dimension R(*),b(*)
       n1=nmax+1
       ii=nn
       b(n)=b(n)/R(nn)
@@ -24,14 +32,17 @@
         ii=ii-n1+i
         b(i)=-scpr(-b(i),R(ii+1),b(i+1),n-i)/R(ii)
       end do
-      return
-      end
+      end subroutine rsol
+!******************************************************************************************
+
+!******************************************************************************************
+!>
+!  solves Rt.x=b with same conventions as above
+!  nn is not required on entry but is set on exit
 
       subroutine rtsol(n,nn,nmax,R,b)
       implicit double precision (a-h,o-z)
       dimension R(*),b(*)
-!  solves Rt.x=b with same conventions as above
-!  nn is not required on entry but is set on exit
       n2=nmax+2
       nn=1
       b(1)=b(1)/R(1)
@@ -41,13 +52,16 @@
         nn=nn+n2-i
         b(i)=b(i)/R(nn)
       end do
-      return
-      end
+      end subroutine rtsol
+!******************************************************************************************
+
+!******************************************************************************************
+!>
+!  forms b=M.x where Q is nxn, stored by columns, with stride nmax
 
       subroutine Qprod(n,nmax,Q,x,b)
       implicit double precision (a-h,o-z)
       dimension Q(*),x(*),b(*)
-!  forms b=M.x where Q is nxn, stored by columns, with stride nmax
       do i=1,n
         b(i)=0.D0
       end do
@@ -56,21 +70,27 @@
         call mysaxpy(x(i),Q(i1),b,n)
         i1=i1+nmax
       end do
-      return
-      end
+      end subroutine Qprod
+!******************************************************************************************
+
+!******************************************************************************************
+!>
+!  forms b=M'.x where Q is nxn, stored by columns, with stride nmax
 
       subroutine Qtprod(n,nmax,Q,x,b)
       implicit double precision (a-h,o-z)
       dimension Q(*),x(*),b(*)
-!  forms b=M'.x where Q is nxn, stored by columns, with stride nmax
       i1=1
       do i=1,n
         b(i)=scpr(0.D0,Q(i1),x,n)
         i1=i1+nmax
       end do
-      return
-      end
+      end subroutine Qtprod
+!******************************************************************************************
 
+!******************************************************************************************
+!>
+!
       subroutine brots(n,nmax,k,kk,R,v)
       implicit double precision (a-h,o-z)
       dimension R(*),v(*)
@@ -85,13 +105,16 @@
         R(ii)=cos*R(ii)
         ipip=ii
       end do
-      return
-      end
+      end subroutine brots
+!******************************************************************************************
+
+!******************************************************************************************
+!>
+! nr is either nc or nc+1
 
       subroutine frots(nr,nc,nmax,R,v)
       implicit double precision (a-h,o-z)
       dimension R(*),v(*)
-! nr is either nc or nc+1
       ii=1
       do i=1,nc
         ip=i+1
@@ -101,24 +124,30 @@
         call rot(nr-i,R(ipi),R(ipip),cos,sin)
         ii=ipip
       end do
-      return
-      end
+      end subroutine frots
+!******************************************************************************************
 
+!******************************************************************************************
+!>
+!
       subroutine angle(a,b,cos,sin)
       implicit double precision (a-h,o-z)
       z=sqrt(a**2+b**2)
       if (z==0.D0) then
         cos=1.D0
         sin=0.D0
-        return
+      else
+        cos=a/z
+        sin=b/z
+        a=z
+        b=0.D0
       end if
-      cos=a/z
-      sin=b/z
-      a=z
-      b=0.D0
-      return
-      end
+      end subroutine angle
+!******************************************************************************************
 
+!******************************************************************************************
+!>
+!
       subroutine rot(n,a,b,cos,sin)
       implicit double precision (a-h,o-z)
       dimension a(*),b(*)
@@ -153,9 +182,12 @@
           b(i)=sin*z-cos*b(i)
         end do
       end if
-      return
-      end
+      end subroutine rot
+!******************************************************************************************
 
+!******************************************************************************************
+!>
+!
       subroutine mysaxpy(a,x,y,n)
       implicit double precision (a-h,o-z)
       dimension x(*),y(*)
@@ -163,12 +195,15 @@
       do i=1,n
         y(i)=y(i)+a*x(i)
       end do
-      return
-      end
+      end subroutine mysaxpy
+!******************************************************************************************
+
+!******************************************************************************************
+!>
+!  saxpy with stride
 
       subroutine saxpys(a,x,is,y,n)
       implicit double precision (a-h,o-z)
-!  saxpy with stride
       dimension x(*),y(*)
       if (a==0.D0) return
       ix=1
@@ -176,12 +211,15 @@
         y(i)=y(i)+a*x(ix)
         ix=ix+is
       end do
-      return
-      end
+      end subroutine saxpys
+!******************************************************************************************
+
+!******************************************************************************************
+!>
+!  saxpy with result in x
 
       subroutine saxpyx(a,x,y,n)
       implicit double precision (a-h,o-z)
-!  saxpy with result in x
       dimension x(*),y(*)
       if (a==0.D0) then
         do i=1,n
@@ -192,12 +230,15 @@
           x(i)=y(i)+a*x(i)
         end do
       end if
-      return
-      end
+      end subroutine saxpyx
+!******************************************************************************************
+
+!******************************************************************************************
+!>
+!  saxpy with result in z
 
       subroutine saxpyz(a,x,y,z,n)
       implicit double precision (a-h,o-z)
-!  saxpy with result in z
       dimension x(*),y(*),z(*)
       if (a==0.D0) then
         do i=1,n
@@ -208,12 +249,15 @@
           z(i)=y(i)+a*x(i)
         end do
       end if
-      return
-      end
+      end subroutine saxpyz
+!******************************************************************************************
+
+!******************************************************************************************
+!>
+!  saxpy with interchange of x and y
 
       subroutine saxpyi(a,x,y,n)
       implicit double precision (a-h,o-z)
-!  saxpy with interchange of x and y
       dimension x(*),y(*)
       if (a==0.D0) then
         do i=1,n
@@ -226,9 +270,12 @@
           x(i)=z
         end do
       end if
-      return
-      end
+      end subroutine saxpyi
+!******************************************************************************************
 
+!******************************************************************************************
+!>
+!
       function scpr(a,x,y,n)
       implicit double precision (a-h,o-z)
       dimension x(*),y(*)
@@ -236,8 +283,9 @@
       do i=1,n
         scpr=scpr+x(i)*y(i)
       end do
-      return
-      end
+      end function scpr
+!******************************************************************************************
+
 
 !     function xlen(a,x,n)
 !     implicit double precision (a-h,o-z)
@@ -278,6 +326,9 @@
 !     return
 !     end
 
+!******************************************************************************************
+!>
+!
       function xlen(a,x,n)
       implicit double precision (a-h,o-z)
       dimension x(*)
@@ -286,9 +337,12 @@
         xlen=xlen+x(i)**2
       end do
       xlen=sqrt(xlen)
-      return
-      end
+      end function xlen
+!******************************************************************************************
 
+!******************************************************************************************
+!>
+!
       subroutine linf(n,x,z,iz)
       implicit double precision (a-h,o-z)
       dimension x(*)
@@ -300,9 +354,12 @@
           iz=i
         end if
       end do
-      return
-      end
+      end subroutine linf
+!******************************************************************************************
 
+!******************************************************************************************
+!>
+!
       subroutine r_shift(r,n,k)
       implicit double precision (a-h,o-z)
       dimension r(*)
@@ -315,9 +372,12 @@
           r(i)=r(i+k)
         end do
       end if
-      return
-      end
+      end subroutine r_shift
+!******************************************************************************************
 
+!******************************************************************************************
+!>
+!
       subroutine ishift(l,n,k)
       implicit double precision (a-h,o-z)
       dimension l(*)
@@ -330,17 +390,23 @@
           l(i)=l(i+k)
         end do
       end if
-      return
-      end
+      end subroutine ishift
+!******************************************************************************************
 
+!******************************************************************************************
+!>
+!
       subroutine rexch(a,b)
       double precision a,b,z
       z=a
       a=b
       b=z
-      return
-      end
+      end subroutine rexch
+!******************************************************************************************
 
+!******************************************************************************************
+!>
+!
       subroutine vexch(a,b,n)
       double precision a,b,z
       dimension a(*),b(*)
@@ -349,12 +415,19 @@
         a(i)=b(i)
         b(i)=z
       end do
-      return
-      end
+      end subroutine vexch
+!******************************************************************************************
 
+!******************************************************************************************
+!>
+!
       subroutine iexch(i,j)
       k=i
       i=j
       j=k
-      return
-      end
+      end subroutine iexch
+!******************************************************************************************
+
+!******************************************************************************************
+!    end module utilities
+!******************************************************************************************
